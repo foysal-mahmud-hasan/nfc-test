9cae959ffe8f30f2098d65fce8618372a1b28518
foysal-mahmud-hasan <1730150@iub.edu.bd>
2025-07-12 14:26:16 +0600

    header cleanup

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,37 +1,16 @@
 import {
-    HoverCard,
     Group,
     Button,
-    UnstyledButton,
     Text,
-    SimpleGrid,
-    ThemeIcon,
-    Anchor,
-    Divider,
-    Center,
     Box,
-    Burger,
-    rem,
     useMantineTheme,
-    Image,
-    Notification, NavLink, Container, Flex, ScrollArea
+    Image,Container, Flex
 } from "@mantine/core";
-import Logo from "../../assets/images/tbd-logo.png";
 
-import { useDisclosure, useFullscreen, useHotkeys } from "@mantine/hooks";
-import {
-    IconNotification,
-    IconCode,
-    IconBook,
-    IconChartPie3,
-    IconFingerprint,
-    IconCoin,
-    IconWifiOff,
-} from "@tabler/icons-react";
-import HeaderStyle from "./../../assets/css/Header.module.css";
+import { useDisclosure} from "@mantine/hooks";
 
 import "@mantine/spotlight/styles.css";
-import React, { useEffect, useState } from "react";
+import  { useState } from "react";
 import flagBD from "../../assets/images/flags/bd.svg";
 import flagGB from "../../assets/images/flags/gb.svg";
 import { useTranslation } from "react-i18next";
@@ -50,19 +29,11 @@ export default function Header() {
     const [opened, { open, close }] = useDisclosure(false);
     const { t, i18n } = useTranslation();
     const navigate = useNavigate();
-    const location = useLocation(); // Add this line to get the current location
-    const theme = useMantineTheme();
-
-    // useEffect(() => {
-    //     console.log('Current location:', location.pathname);
-    // }, [location]);
+    const location = useLocation();
 
     const [isClicked, setIsClicked] = useState(false);
     const path = '/sign-up'
-    const pa = location.pathname;
     const handleClick = () => {
-        console.log(path)
-        console.log(pa);
         navigate(path);
     };
 
@@ -100,7 +71,6 @@ export default function Header() {
                             </Button>
                         )}                        
                 </Group>
-
             </Box>
         </Container>
     );
